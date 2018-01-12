1f79ed7c09e4f0a72597abf71550e5b843ea5c9e
aran.hu <1903283729@qq.com>
2018-01-12 14:01:56 +0800

    save files

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const Koa = require('koa');
 const app = new Koa();
 const { uploadFile } = require('./upload')
+const path = require('path')
+const koaBody = require('koa-body');
+const fs = require('fs')
 
 // response
 // app.use(ctx => {
@@ -9,38 +12,54 @@ const { uploadFile } = require('./upload')
 //   ctx.body = 'Hello Koa';
 // });
 
+app.use(koaBody({ multipart: true }));
+app.use(async function (ctx, next) {
+  if ('POST' != ctx.method) return await next();
+  // console.log(ctx.request.body) //base 64了
+  let base64Data = ctx.request.body
+  var dataBuffer = new Buffer(base64Data, 'base64');
+  fs.writeFile("image.png", dataBuffer, function (err) {
+    if (err) {
+      console.log('失败', err)
+    } else {
+      console.log('成功')
+    }
+  });
+  ctx.body = "success";
+});
 
-app.use( async ( ctx ) => {
-  if ( ctx.url === '/' && ctx.method === 'GET' ) {
-    // 当GET请求时候返回表单页面
-    let html = `
-      <h1>koa2 upload demo</h1>
-      <form method="POST" action="/upload.json" enctype="multipart/form-data">
-        <p>file upload</p>
-        <span>picName:</span><input name="picName" type="text" /><br/>
-        <input name="file" type="file" /><br/><br/>
-        <button type="submit">submit</button>
-      </form>
-    `
-    ctx.body = html
+// app.use( async ( ctx ) => {
+//   if ( ctx.url === '/' && ctx.method === 'GET' ) {
+//     // 当GET请求时候返回表单页面
+//     let html = `
+//       <h1>koa2 upload demo</h1>
+//       <form method="POST" action="/upload.json" enctype="multipart/form-data">
+//         <p>file upload</p>
+//         <span>picName:</span><input name="picName" type="text" /><br/>
+//         <input name="file" type="file" /><br/><br/>
+//         <button type="submit">submit</button>
+//       </form>
+//     `
+//     ctx.body = html
 
-  } else if ( ctx.url === '/upload.json' && ctx.method === 'POST' ) {
-    // 上传文件请求处理
-    let result = { success: false }
-    let serverFilePath = path.join( __dirname, 'upload-files' )
+//   } else if ( ctx.url === '/upload.json' && ctx.method === 'POST' ) {
+//     // 上传文件请求处理
+//     let result = { success: false }
+//     let serverFilePath = path.join( __dirname, 'upload-files' )
 
-    // 上传文件事件
-    result = await uploadFile( ctx, {
-      fileType: 'album', // common or album
-      path: serverFilePath
-    })
+//     console.log(ctx.request.body.files.file)
+//     // 上传文件事件
+//     // result = await uploadFile( ctx, {
+//     //   fileType: 'album', // common or album
+//     //   path: serverFilePath
+//     // })
 
-    ctx.body = result
-  } else {
-    // 其他请求显示404
-    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
-  }
-})
+//     ctx.body = result
+//   } else {
+//     // 其他请求显示404
+//     ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+//   }
+// })
 
 app.listen(3002, () => {
   console.log('[demo] jsonp is starting at port 3002')
